30a81168e53f13f35ae5f6c4a262ddc8e8c1a1f5
Park18 <sowoomi35@naver.com>
2019-12-15 17:38:15 +0900

    테스트

---
+++ src/Server.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(5555);
+        Socket socket = serverSocket.accept();
+
+        System.out.println("client connected");
+
+        InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String string = bufferedReader.readLine();
+        System.out.println("client: " + string);
+    }
+}
